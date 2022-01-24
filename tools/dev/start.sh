2a925d6bea89d9c0d33d4ac996b781f94ad554fd
Emmanuel <emmanuelm41@gmail.com>
2022-01-24 12:33:20 -0300

    add butterfly and calibration images
    - re estructure the repo to support both new images

--- tools/dev/start.sh
+++ tools/dev/start.sh
@@ -6,7 +6,7 @@ NC='\033[0m'
 mkdir -p /root/.lotus && cp -fr /configFiles/* /root/.lotus
 chmod 0600 -R /root/.lotus
 
-lotus daemon --config /devnet_config.toml --lotus-make-genesis=devgen.car --genesis-template=localnet.json --bootstrap=false&
+lotus daemon --config /config.toml --lotus-make-genesis=devgen.car --genesis-template=localnet.json --bootstrap=false&
 sleep 5
 
 # Import test actors
