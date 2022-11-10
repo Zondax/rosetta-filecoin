0aab6b4415ff3598985c2cdc1cce98e5c39f5d81
Nicolas Camilo <58498506+negarciacamilo@users.noreply.github.com>
2022-11-10 10:11:30 -0300

    Added some comments

--- tools/dev/files/start.sh
+++ tools/dev/files/start.sh
@@ -4,7 +4,9 @@ echo "LOTUS | Starting..."
 sh ./start_lotus.sh
 echo "LOTUS | Started!"
 
+echo "Starting mock accounts..."
 sh ./mock_accounts.sh
+echo "Done executing mock accounts"
 
 
 echo "LOTUS | Finished setting up"
