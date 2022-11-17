541d1929d4d0a050c2923b991b6e8d0103bc5005
Nicolas Camilo <58498506+negarciacamilo@users.noreply.github.com>
2022-11-17 14:22:17 -0300

    Replaced old start.sh

--- tools/calibration/files/start.sh
+++ tools/calibration/files/start.sh
@@ -1,7 +1,9 @@
 #! /bin/bash
 
-echo "LOTUS | Starting..."
-sh ./start_lotus.sh
-echo "LOTUS | Started!"
+export LOTUS_VM_ENABLE_TRACING=1
 
+lotus daemon --config /etc/lotus_config/config.toml &
+sleep 5
 
+echo -e "${GREEN}### Launching filecoin-indexing-rosetta-proxy${NC}"
+filecoin-indexing-rosetta-proxy 2>&1
